2a5c5dc51eee1bc971eb39fecac241403e97abef
prsmart2150@gmail.com <praveenprajapati0810@gmail.com>
2023-03-03 16:22:05 +0530

    solve the cors issue

--- src/redux/Axios.js
+++ src/redux/Axios.js
@@ -4,14 +4,14 @@ const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
 });
 
-instance.interceptors.request.use((req) => {
-  if (localStorage.getItem('profile')) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem('profile')).token
-      // 'moRDqHouGPKDZj9AojHdv2uZhvxysygh'
-    }`;
-  }
-  return req;
-});
+// instance.interceptors.request.use((req) => {
+//   if (localStorage.getItem('profile')) {
+//     req.headers.Authorization = `Bearer ${
+//       JSON.parse(localStorage.getItem('profile')).token
+//       // 'moRDqHouGPKDZj9AojHdv2uZhvxysygh'
+//     }`;
+//   }
+//   return req;
+// });
 
 export default instance;
